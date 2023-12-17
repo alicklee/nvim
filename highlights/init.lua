93aeaa2e5242fe261cf51bd68350864d48ba8f41
alicklee <libo1125@gmail.com>
2023-12-18 07:44:24 +0800

    save

--- highlights/init.lua
+++ highlights/init.lua
@@ -1,3 +1,4 @@
 return { -- this table overrides highlights in all themes
   -- Normal = { bg = "#000000" },
+  -- https://subapi.rss-node.com/sub?target=clash&interval=129600&filename=Hitun&url=https%3A%2F%2Frss-node.com%2Flink%2FwUK48d6Dja6F06WO%3Fmu%3D1
 }
